52f97e6f9f9f6904c6ada8290f756ab9bbf84a06
Brandon Ellis <brandonellis@Brandons-MacBook-Pro.local>
2019-10-29 20:15:24 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '/Users/brandonellis/Documents/Code Projects/weather-app/weather-app/src/App.css';
+import Weather from '/Users/brandonellis/Documents/Code Projects/weather-app/weather-app/src/components/weather.component.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
 
-function App() {
-  return (
+
+const apiBase = "http://api.openweathermap.org/data/2.5/weather?";
+const apiKey = "32710cb36385401192dab72b89409cea";
+
+//default value is metric; other value is imperial
+const units = "metric";
+
+// default units is metric. If button is pushed, change value to 'imperial'
+
+class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+
+  getWeather = async () =>  {
+    const apiCall = fetch(`${apiBase}q=London,uk&units=${units}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(function(res) {
+      let weather = res["weather"][0]["main"];
+      console.log(weather);
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather /> 
     </div>
-  );
+    );
+  }
 }
 
 export default App;
