9939fd8f217b6282f78b1afd8744a1467ab37077
Brandon Ellis <brandonellis@Brandons-MacBook-Pro.local>
2019-11-07 16:39:29 -0700

    setting up feature to remove item

--- src/components/savedWeather.jsx
+++ src/components/savedWeather.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const SavedWeather = (props) => {
 
+    const styles = {
+        display: "flex"
+    }
+
     if (props.name !== "") {
     
     return (
-        <div className="savedWeatherList">
-            <p className="savedItem" onClick={() => props.useSaved(props.name, props.country)} >{props.name}, {props.country}</p>
+        <div style={styles} className="savedWeatherList">
+            <p className="savedItem" onClick={() => props.useSaved(props.name, props.country)} >{props.name}, {props.country} <span id="remove-saved-btn">-</span></p>
         </div>
     )
 
